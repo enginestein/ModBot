51450537ad53739ba5665281d89d6f8856fccaf9
Neil <aryapraneil@gmail.com>
2025-03-22 08:46:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -650,21 +650,21 @@ async def on_presence_update(before, after):
 @bot.event
 async def on_message(message):
 
-    if bot.user.mentioned_in(message):
-        print(f"[DEBUG] Bot was mentioned by {message.author.id}")
+    if bot.user.mentioned_in(message) and not message.mention_everyone:
+      print(f"[DEBUG] Bot was mentioned by {message.author.id}")
 
-        if message.author.id == SPECIAL_USER_ID:
-            special_message1 = random.choice(special_messages)
-            await message.channel.send(special_message1)
-        elif message.author.id == SECONDARY_SPECIAL_USER_ID:
-            special_message2 = random.choice(secondary_messages)
-            await message.channel.send(special_message2)
-        elif message.author.id == TERTIARY_SPECIAL_USER_ID:
-            special_message3 = random.choice(tertiary_messages)
-            await message.channel.send(special_message3)
-        else:
-            random_message = random.choice(random_messages)
-            await message.channel.send(random_message)
+      if message.author.id == SPECIAL_USER_ID:
+          special_message1 = random.choice(special_messages)
+          await message.channel.send(special_message1)
+      elif message.author.id == SECONDARY_SPECIAL_USER_ID:
+          special_message2 = random.choice(secondary_messages)
+          await message.channel.send(special_message2)
+      elif message.author.id == TERTIARY_SPECIAL_USER_ID:
+          special_message3 = random.choice(tertiary_messages)
+          await message.channel.send(special_message3)
+      else:
+          random_message = random.choice(random_messages)
+          await message.channel.send(random_message)
 
     
     if message.author.bot:
